ea7dd585a4c2ad4b43ff72dbe2caa9479bbe71f0
BuiKiet <roiyano1utl@gmail.com>
2023-03-03 21:19:59 +0700

    Start Projest

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="css/style.css">
+    <title><?php
+        echo 'FOOTBALL ORDER IN CANTHO CITY'
+	?></title>
+
+
+</head>
