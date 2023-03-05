16db17bf8d344f4d2f2289e26150132f092860fb
BuiKiet <roiyano1utl@gmail.com>
2023-03-05 14:48:04 +0700

    fix

--- mvc/views/home.php
+++ mvc/views/home.php
@@ -1,56 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Football Order</title>
-    <link rel="stylesheet" href="./assets/css/home.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Football Order</title>
 
-<body>
-    <div id="root">
-        <header id="header">
-            <div class="header__nav">
-                <div class="header__nav--logo">
-                    <img src="./assets/images/logo.jpg" alt="FootBall" class="img">
-                </div>
 
-                <div class="header__list">
-                    <ul class="header__list--nav">
-                        <li class="header__list--item">
-                            <a href="#" class='header__item--link'>Trang chủ</a>
-                        </li>
-                        <li class="header__list--item">
-                            <a href="#" class='header__item--link'>Giới thiệu</a>
-                        </li>
-                        <li class="header__list--item">
-                            <a href="#" class='header__item--link'>Đặt sân</a>
-                        </li>
-                        <li class="header__list--item">
-                            <a href="#" class='header__item--link'>Đánh giá</a>
-                        </li>
-                        <li class="header__list--item">
-                            <a href="#" class='header__item--link'>Liên hệ</a>
-                        </li>
-                    </ul>
-                    <div class="authentication">
-                        <div>Đăng nhập</div>
+        <!-- Icon -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+
+        <!-- Boots Chap 5-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <!-- Css -->
+        <link rel="stylesheet" href="./assets/css/index.css">
+        <link rel="stylesheet" href="./assets/css/home.css">
+        <link rel="stylesheet" href="./assets/css/contact.css">
+
+    </head>
+
+    <body>
+        <div id="root">
+            <header id="header">
+                <div class="header__nav container">
+                    <div class="header__nav--logo">
+                        <img src="./assets/images/logo.jpg" alt="FootBall" class="img">
                     </div>
+
+                    <div id="header__list">
+                        <ul class="header__list--nav">
+                            <li class="header__list--item">
+                                <a href="#" class='header__item--link'>Trang chủ</a>
+                            </li>
+                            <li class="header__list--item">
+                                <a href="#" class='header__item--link'>Giới thiệu</a>
+                            </li>
+                            <li class="header__list--item">
+                                <a href="#" class='header__item--link'>Đặt sân</a>
+                            </li>
+                            <li class="header__list--item">
+                                <a href="#" class='header__item--link'>Đánh giá</a>
+                            </li>
+                            <li class="header__list--item">
+                                <a href="#" class='header__item--link'>Liên hệ</a>
+                            </li>
+
+                        </ul>
+                        <div class="authentication">
+                            <div>Đăng nhập</div>
+                        </div>
+                    </div>
+
                 </div>
+            </header>
 
-            </div>
-        </header>
+            <main id="main">
+                <h1>HOME PAGE!</h1>
+            </main>
 
-        <main id="main">
-            <h1>HOME PAGE!</h1>
-        </main>
-
-        <footer id="footer">
-            <h1>FOOTER!</h1>
-        </footer>
-    </div>
-</body>
+            <footer id="footer">
+                <h1>FOOTER!</h1>
+            </footer>
+        </div>
+    </body>
 
 </html>
