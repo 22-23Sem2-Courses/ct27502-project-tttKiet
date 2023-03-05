c9b41ad738adb1c6f1363332ce957d62c90136dd
BuiKiet <roiyano1utl@gmail.com>
2023-03-05 15:01:17 +0700

    3. first header

--- mvc/views/home.php
+++ mvc/views/home.php
@@ -18,43 +18,15 @@
         <!-- Css -->
         <link rel="stylesheet" href="./assets/css/index.css">
         <link rel="stylesheet" href="./assets/css/home.css">
-        <link rel="stylesheet" href="./assets/css/contact.css">
 
     </head>
 
     <body>
         <div id="root">
             <header id="header">
-                <div class="header__nav container">
-                    <div class="header__nav--logo">
-                        <img src="./assets/images/logo.jpg" alt="FootBall" class="img">
-                    </div>
-
-                    <div id="header__list">
-                        <ul class="header__list--nav">
-                            <li class="header__list--item">
-                                <a href="#" class='header__item--link'>Trang chủ</a>
-                            </li>
-                            <li class="header__list--item">
-                                <a href="#" class='header__item--link'>Giới thiệu</a>
-                            </li>
-                            <li class="header__list--item">
-                                <a href="#" class='header__item--link'>Đặt sân</a>
-                            </li>
-                            <li class="header__list--item">
-                                <a href="#" class='header__item--link'>Đánh giá</a>
-                            </li>
-                            <li class="header__list--item">
-                                <a href="#" class='header__item--link'>Liên hệ</a>
-                            </li>
-
-                        </ul>
-                        <div class="authentication">
-                            <div>Đăng nhập</div>
-                        </div>
-                    </div>
-
-                </div>
+                <?php 
+                    include "./partials/header-nav.php"
+                ?>
             </header>
 
             <main id="main">
