47bc32336af38da99eb81008d32effa250549da6
fcthevan123-vn <fcthevan123@gmail.com>
2023-03-07 21:26:32 +0700

    fix footer and processing feeback

--- public/partials/footer.php
+++ public/partials/footer.php
@@ -1,14 +1,6 @@
 <div class="footer-wrap">
     <div class="container pb-2 pt-3">
-        <div class="row ">
-            <div class="list-link text-center">
-                <a href="" class="link-item">Trang chủ</a>
-                <a href="" class="link-item">Giới thiệu</a>
-                <a href="" class="link-item">Đặt sân</a>
-                <a href="" class="link-item">Đánh giá</a>
-                <a href="" class="link-item">Đăng nhập</a>
-            </div>
-        </div>
+
         <hr>
         <div class="row">
             <div class="col">
