e9cd585b14eccf33868919257c93a4a8acd2f61f
BuiKiet <roiyano1utl@gmail.com>
2023-04-10 19:42:32 +0700

    * last

---
+++ libraries/bridge.php
@@ -0,0 +1,12 @@
+<?php
+
+    //  Required app 
+    require_once '../mvc/core/App.php';
+
+    // Sử dụng controllers để gọi models và views
+    require_once '../mvc/core/Controller.php';
+
+    // Kết nối Database
+    require_once "../mvc/core/DB.php";
+    
+?>
